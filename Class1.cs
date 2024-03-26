9818556264ca834f2c2ecbb09cd06532c6d578ad
ANDR <ANDR@APOLO>
2024-03-26 21:42:18 +0200

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿// Использование класса Country
+using Pr_1_class;
+
+var country = new Country("Narnia", 150000, 4500000);
+
+country.DisplayInfo();
+
+country.UpdatePopulation(5000000);
+country.DisplayInfo();
+
+if (country.IsOverpopulated())
+{
+    Console.WriteLine("Yes, it's overpopulated.");
+    country.ReducePopulation();
+    country.DisplayInfo();
+}
+else
+{
+    Console.WriteLine("No, it's not overpopulated.");
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Pr_1_class;
+using System;
+
+namespace Pr_1_class
+{
+    public class Country
+    {
+        private string name;
+        private double area; 
+        private int population; 
+
+        public Country(string name, double area, int population)
+        {
+            this.name = name;
+            this.area = area;
+            this.population = population;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Country: {name}");
+            Console.WriteLine($"Area: {area} square kilometers");
+            Console.WriteLine($"Population: {population} people");
+        }
+
+        public void UpdatePopulation(int population)
+        {
+            this.population = population;
+            Console.WriteLine($"{name}'s population has been updated.");
+        }
+
+        public bool IsOverpopulated()
+        {
+            return population / area > 250; // Простой пример, когда считаем страну перенаселенной
+        }
+
+        public void ReducePopulation()
+        {
+            population = (int)(population * 0.9); // Уменьшаем население на 10%
+            Console.WriteLine($"The population of {name} has been reduced.");
+        }
+    }
+}
+
